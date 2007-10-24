5b84130d307523acb682d6f194fd3de90432c47c
migue <migue@localhost.localdomain>
2007-10-25 00:04:19 +0200

    .

--- tvscrap.py
+++ tvscrap.py
@@ -5,6 +5,13 @@ from BeautifulSoup import BeautifulSoup
 import re
 from eztvefnet import Scrapper
 
+# ideas:
+# + tvscrap.py --register-show="Show" --regexp="rx" --min-size=xx --max-size=xx
+# tvscrap.ini
+# [torrents]
+# dir=/home/migue/download/torrents
+#
+
 if __name__ == '__main__':
     s = Scrapper()
     res = s()
