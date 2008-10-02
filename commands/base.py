d65890c476067511609b7c7fb4affd4d9dc3012f
migue <migue@nano>
2008-10-03 01:16:10 +0200

    refactorizando comandos

---
+++ commands/base.py
@@ -0,0 +1,12 @@
+# tvscrap help
+# tvscrap register -r <show> -x <rx> [-m xx] [-n xx]
+# tvscrap shows
+# tvscrap episodes <show>
+# tvscrap delete <show> <episode>
+# tvscrap eztv [-f file|-u url]
+class Command(object):
+    
+    def run(self):
+        pass
+
+
