6fb2c81327eca2f07682525c3f8c5ca099bd6566
enlavin <devnull@localhost>
2008-10-06 23:12:24 +0200

    nuevos comandos para manejar variables de configuracion

--- commands/help.py
+++ commands/help.py
@@ -14,18 +14,24 @@ class Command(BaseCommand):
         print """
 tvscrap help
     Show this help
-tvscrap register -r <show> -x <rx> [-m xx] [-n xx]
+tvscrap register -s <show> -x <rx> [-m xx] [-n xx]
     Register a new show in DB
 tvscrap shows
     List of registered shows
 tvscrap episodes <show>
     Episode list for a show
 tvscrap delete -s <show> [-e <episode>]
-    Delete an episode from DB
+    Delete an episode/show from DB
 tvscrap pending
     List of pending episodes
 tvscrap eztv [-f file|-u url]
     Download torrents from eztv
-tvscrap mldonkey [-h host] [-p port] [-u username] [-w password]
+tvscrap mldonkey [-m host] [-p port] [-u username] [-w password]
     Queue torrents in mldonkey
+tvscrap config
+    Dump config variables
+tvscrap set -n varname -v value
+    Set/Update config variable
+tvscrap unset -n varname
+    Delete config variable
         """
